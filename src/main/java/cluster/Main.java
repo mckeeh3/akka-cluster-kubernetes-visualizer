660b16b11fcb0704ecd35740df8a3865ab0d9b97
Hugh McKee <mckeeh3@gmail.com>
2020-10-26 15:52:29 -0400

    working on docs and crc deploy

--- src/main/java/cluster/Main.java
+++ src/main/java/cluster/Main.java
@@ -3,17 +3,25 @@ package cluster;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.Terminated;
+import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.management.cluster.bootstrap.ClusterBootstrap;
 import akka.management.javadsl.AkkaManagement;
 
 class Main {
+
   static Behavior<Void> create() {
-    return Behaviors.setup(
-        context -> Behaviors.receive(Void.class)
+    return Behaviors.setup(context -> {
+          bootstrap(context);
+
+          return Behaviors.receive(Void.class)
             .onSignal(Terminated.class, signal -> Behaviors.stopped())
-            .build()
-    );
+            .build();
+        });
+  }
+
+  private static void bootstrap(final ActorContext<Void> context) {
+    context.spawn(ClusterListenerActor.create(), "clusterListener");
   }
 
   public static void main(String[] args) {
